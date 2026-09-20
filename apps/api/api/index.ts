d70ae43b0e307a15c5abc6e41ff3ed696f391ecb
Tuan Tai <buituantai2k4@gmail.com>
2026-09-20 20:48:44 +0700

    no commit

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,5 +1,14 @@
 export default async function (req: any, res: any) {
-  const mod = require('../dist/main');
-  const handler = mod.default || mod;
-  return await handler(req, res);
+  try {
+    const mod = require('../dist/main');
+    const handler = mod.default || mod;
+    return await handler(req, res);
+  } catch (err: any) {
+    console.error('Vercel API Index Error:', err);
+    res.status(500).json({
+      error: 'Vercel API Index Require Failed',
+      message: err.message,
+      stack: err.stack
+    });
+  }
 }
